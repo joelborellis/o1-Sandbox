e7894906d74a3caaf3c0a3ae23d4b1baa0e2a849
Joel Borellis <joelborellis@outlook.com>
2024-10-03 15:08:36 -0500

    added streamlit and the planning task

--- app/Chat.py
+++ app/Chat.py
@@ -14,8 +14,6 @@ OPENAI_MODEL = os.environ.get("OPENAI_MODEL")
 # Azure OpenAI key from .env file
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
-st.title("Chat with o1-preview")
-
 # Set OpenAI API key from Streamlit secrets
 client = AsyncOpenAI(api_key=OPENAI_API_KEY)
 
@@ -61,45 +59,71 @@ async def main():
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+    # Display chat messages from history on app rerun which is when chat_input is clicked
+    # for message in st.session_state.messages:
+    #    with st.chat_message(message["role"]):
+    #        st.markdown(message["content"])
 
     # Accept user input
-    if prompt := st.chat_input("What is up?"):
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": f"{prompt}.  Always return your response in markdown format"})
-        # Display user message in chat message container
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Display assistant response in chat message container
-
-        with st.chat_message("assistant"):
-            text, tokens, formatted_time = await chat(
-                model=OPENAI_MODEL,
-                messages=[
-                    {"role": m["role"], "content": m["content"]}
-                    # {"role": m["role"], "content": [{"type": "text", "text": m["content"]}]}
-                    for m in st.session_state.messages
-                ],
-                stream=False,
-                max_completion_tokens=4000,
-                temperature=1,
+    with st.sidebar:
+        st.header("Reason with o1-preview", divider="gray")
+        messages = st.container(height=300)
+        if prompt := st.chat_input("Say something"):
+            # Here is there we will add the token stuff
+            messages.chat_message("user").write(prompt)
+            st.session_state.messages.append(
+                {
+                    "role": "user",
+                    "content": f"{prompt}.  Always return your response in markdown format",
+                }
             )
-            text_md = markdown.markdown(text) # the string text converted to true markdown
+    # Display user message in chat message container
+
+    # with st.chat_message("user"):
+    # st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    if st.session_state.messages:
+        with st.status("Reasoning and thinking...", expanded=False) as status:
+        #with st.status("Reasoning and thinking...", expanded="True"):
+            #st.write(st.session_state.tokens)
+            #with st.container():
+                # with st.chat_message("assistant"):
+            text, tokens, formatted_time = await chat(
+                    model=OPENAI_MODEL,
+                    messages=[
+                        {"role": m["role"], "content": m["content"]}
+                        # {"role": m["role"], "content": [{"type": "text", "text": m["content"]}]}
+                        for m in st.session_state.messages
+                    ],
+                    stream=False,
+                    max_completion_tokens=4000,
+                    temperature=1,
+                )
+            status.update(
+                    label="Complete!", state="complete", expanded=True
+                    )
+            #st.write(tokens)
+            st.write(f"Your question took a total of: {tokens.total_tokens} tokens")
+            st.write(f"Your question took: {tokens.completion_tokens_details.reasoning_tokens} reasoning tokens")
+            st.write(f"Your question prompt used: {tokens.prompt_tokens_details}")
+            st.write(formatted_time)
+            #st.session_state.tokens.append(tokens)
+            #print(
+            #        f"Reasoning tokens:  {tokens.completion_tokens_details.reasoning_tokens}"
+            #    )
+            # text_md = markdown.markdown(text) # the string text converted to true markdown
             # Initialize a placeholder for streaming
-            placeholder = st.empty()           
+            # placeholder = st.empty()
             # Stream the response as markdown
-            markdown_text = ""
-            for chunk in response_generator(text_md):
-                markdown_text += chunk
-                placeholder.html(markdown_text)  # Update the placeholder with streamed markdown
-            #response = (st.write_stream(response_generator(text_md)))
-            #response = st.write(text)
-        # st.session_state.messages.append({"role": "assistant", "content": response})
-        st.session_state.messages.append({"role": "user", "content": text})
+            # markdown_text = ""
+            # for chunk in response_generator(text_md.replace("<h1>", "<h2>").replace("</h1>", "</h2>").replace("<h2>", "<h3>").replace("</h2>", "</h3>")):
+            #    markdown_text += chunk
+            #    placeholder.html(markdown_text)  # Update the placeholder with streamed markdown
+            # response = (st.write_stream(response_generator(text_md)))
+            st.markdown(text)
+            # st.session_state.messages.append({"role": "assistant", "content": response})
+            st.session_state.messages.append({"role": "assistant", "content": text})
 
 
 # Run the asynchronous main function
